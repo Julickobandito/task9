212ccc8303c96d28ad385668b19eb1f2dd9a48cd
Julia K <belatrissa.grandger@gmail.com>
2020-12-22 03:46:43 +0000

    Add files

--- finder.sh
+++ finder.sh
@@ -1,7 +1,8 @@
+
 #! /bin/bash
 
 QUERY=$1
 
-find ./folder/ -type f -printf "%f/n" | grep "$QUERY"
+find ./folder/ -type f -printf "%f\n" | grep "$QUERY"
 
-echo "POSTAVTE 100 PLS))))"
+echo "search is over"
